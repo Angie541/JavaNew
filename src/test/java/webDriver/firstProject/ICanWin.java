1d8ff9332cb6d13eec0d1dfde0f7ab062a072e67
Angie541 <66465004+Angie541@users.noreply.github.com>
2021-01-14 10:19:26 +0100

    Update ICanWin.java

--- src/test/java/webDriver/firstProject/ICanWin.java
+++ src/test/java/webDriver/firstProject/ICanWin.java
@@ -2,10 +2,20 @@ package  webDriver.firstProject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ICanWin {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+    
+    @BeforeMethod(alwaysRun = true)
+     public void browserSetup() {
+        driver = new ChromeDriver();
+    }
+    
+     @Test
+      public void checkingForDetails() {
         driver.get("https://pastebin.com");
         ICanWinPageElements pageElements = new ICanWinPageElements(driver);
 
@@ -14,8 +24,11 @@ public class ICanWin {
         pageElements.choosePasteExpiration();
         pageElements.inputPasteName();
         pageElements.createNewPaste();
+     }
 
-        Thread.sleep(10000);
+      @AfterMethod(alwaysRun = true)
+       public void closeBrowser() {
         driver.quit();
+        driver = null;
     }
 }
