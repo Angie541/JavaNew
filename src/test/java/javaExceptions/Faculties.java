8e1b8fa51c04eff0ce9bf8c6735e77340daff7c5
Angie541 <66465004+Angie541@users.noreply.github.com>
2021-01-14 11:06:06 +0100

    Update Faculties.java

--- src/test/java/javaExceptions/Faculties.java
+++ src/test/java/javaExceptions/Faculties.java
@@ -1,6 +1,16 @@
 package javaExceptions;
 
 class Faculties extends JavaExceptions {
+    protected String facultyName;
+    
     public Faculties() {
+    }
+    
+    public Faculties(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getFacultyName(){
+        return facultyName;
     }
 }
