fd3481cb2d3ec3aa2d2692ce7ae20e2588cc0470
Angie541 <66465004+Angie541@users.noreply.github.com>
2021-01-14 10:43:02 +0100

    Update BringItOnPageElements.java

--- src/test/java/webDriver/secondProject/BringItOnPageElements.java
+++ src/test/java/webDriver/secondProject/BringItOnPageElements.java
@@ -7,57 +7,59 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BringItOnPageElements extends BringItOnTest{
     public WebDriver driver;
-    public BringItOnPageElements(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-        this.driver = driver;
-    }
-
+    
     @FindBy(xpath = "//*[contains(@id, 'postform-text')]")
     private WebElement newPasteInput;
+    
+    @FindBy(xpath = "//*[contains(@id, 'select2-postform-format-container')] [text()='None']")
+    private WebElement syntaxHighlightingDropdownMenu;
+    
+    @FindBy(xpath = "//*[contains(@class,'select2-results__option')] [text()='Bash']")
+    private WebElement syntaxHighlightingBash;
+    
+    @FindBy(xpath = "//*[contains(@id,'select2-postform-expiration-container')] [text()='Never']")
+    private WebElement pasteExpirationDropdownMenu;
+    
+    @FindBy(xpath = "//*[contains(@class,'select2-results__option')] [text()='10 Minutes']")
+    private WebElement pasteExpirationTenMinutes;
+    
+    @FindBy(xpath = "//*[contains(@id,'postform-name')]")
+    private WebElement pasteNameInput;
+    
+    @FindBy(xpath = "//*[contains(@class, 'btn -big')]")
+    private WebElement newPasteButton;
+
+    public BringItOnPageElements(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
 
     public void inputNewPaste(String string) {
         newPasteInput.sendKeys(string);
     }
 
-   @FindBy(xpath = "//*[contains(@id, 'select2-postform-format-container')] [text()='None']")
-    private WebElement syntaxHighlightingDropdownMenu;
-
     public void openSyntaxHighlightingDropdownMenu() {
         syntaxHighlightingDropdownMenu.click();
     }
 
-    @FindBy(xpath = "//*[contains(@class,'select2-results__option')] [text()='Bash']")
-    private WebElement syntaxHighlighting;
-
     public void chooseSyntaxHighlighting() {
-        syntaxHighlighting.click();
+        syntaxHighlightingBash.click();
     }
-
-    @FindBy(xpath = "//*[contains(@id,'select2-postform-expiration-container')] [text()='Never']")
-    private WebElement pasteExpirationDropdownMenu;
 
     public void openPasteExpirationDropdownMenu() {
         pasteExpirationDropdownMenu.click();
     }
 
-    @FindBy(xpath = "//*[contains(@class,'select2-results__option')] [text()='10 Minutes']")
-    private WebElement pasteExpiration;
-
     public void choosePasteExpiration() {
-        pasteExpiration.click();
+        pasteExpirationTenMinutes.click();
     }
-
-    @FindBy(xpath = "//*[contains(@id,'postform-name')]")
-    private WebElement pasteNameInput;
 
     public void inputPasteName(String string) {
         pasteNameInput.sendKeys(string);
     }
 
-    @FindBy(xpath = "//*[contains(@class, 'btn -big')]")
-    private WebElement newPasteButton;
-
-    public void createNewPasteButton() {
+    public BringItOnTest createNewPasteButton() {
         newPasteButton.click();
+        return new BringItOnTest(driver);
     }
 }
