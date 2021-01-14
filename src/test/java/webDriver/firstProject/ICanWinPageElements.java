4f9369298fdd8986022a4fe9552b6f90b9f68660
Angie541 <66465004+Angie541@users.noreply.github.com>
2021-01-14 10:24:13 +0100

    Update ICanWinPageElements.java

--- src/test/java/webDriver/firstProject/ICanWinPageElements.java
+++ src/test/java/webDriver/firstProject/ICanWinPageElements.java
@@ -7,41 +7,42 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ICanWinPageElements extends ICanWin{
     public WebDriver driver;
+    
+    @FindBy(xpath = "//*[contains(@id, 'postform-text')]")
+    private WebElement newPasteInput;
+    
+    @FindBy(xpath = "//*[contains(@id, 'select2-postform-expiration-container')] [contains(text(), 'Never')]")
+    private WebElement pasteExpirationSelector;
+    
+    @FindBy(xpath = "//*[contains(@class, 'select2-results__option')] [contains(text(), '10 Minutes')]")
+    private WebElement pasteExpirationTenMinutes;
+    
+    @FindBy(xpath = "//*[contains(@id, 'postform-name')]")
+    private WebElement pasteNameInput;
+    
+    @FindBy(xpath = "//*[contains(@class, 'btn -big')]")
+    private WebElement createNewPasteButton;
+    
     public ICanWinPageElements(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
-    @FindBy(xpath = "//*[contains(@id, 'postform-text')]")
-    private WebElement newPasteInput;
-
     public void inputNewPaste(String string) {
         newPasteInput.sendKeys(string);
     }
-
-    @FindBy(xpath = "//*[contains(@id, 'select2-postform-expiration-container')] [contains(text(), 'Never')]")
-    private WebElement pasteExpirationSelector;
 
     public void openMenuOfPasteExpiration() {
         pasteExpirationSelector.click();
     }
 
-    @FindBy(xpath = "//*[contains(@class, 'select2-results__option')] [contains(text(), '10 Minutes')]")
-    private WebElement pasteExpirationButton;
-
     public void choosePasteExpiration() {
-        pasteExpirationButton.click();
+        pasteExpirationTenMinutes.click();
     }
-
-    @FindBy(xpath = "//*[contains(@id, 'postform-name')]")
-    private WebElement pasteNameInput;
 
     public void inputPasteName() {
         pasteNameInput.sendKeys("helloweb");
     }
-
-    @FindBy(xpath = "//*[contains(@class, 'btn -big')]")
-    private WebElement createNewPasteButton;
 
     public void createNewPaste() {
         createNewPasteButton.click();
