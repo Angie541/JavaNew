046b6f0065ddc59bb353d987548e534f581029fb
Angie541 <66465004+Angie541@users.noreply.github.com>
2021-02-10 12:41:33 +0100

    Update ICanWinPageElements.java

--- src/test/java/webDriver/firstProject/ICanWinPageElements.java
+++ src/test/java/webDriver/firstProject/ICanWinPageElements.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ICanWinPageElements extends ICanWin{
+public class ICanWinPageElements {
     public WebDriver driver;
     
     @FindBy(xpath = "//*[contains(@id, 'postform-text')]")
